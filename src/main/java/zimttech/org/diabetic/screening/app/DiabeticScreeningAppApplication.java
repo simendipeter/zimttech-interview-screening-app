fbaa239f4d5a5e651ab166b00b37e1e945cda0f1
Simendi Peter <simendipeter@gmail.com>
2023-05-08 14:17:39 +0200

    initial commit

---
+++ src/main/java/zimttech/org/diabetic/screening/app/DiabeticScreeningAppApplication.java
@@ -0,0 +1,13 @@
+package zimttech.org.diabetic.screening.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiabeticScreeningAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiabeticScreeningAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/zimttech/org/diabetic/screening/app/DiabeticScreeningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package zimttech.org.diabetic.screening.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiabeticScreeningAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
