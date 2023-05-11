9f295350db765a864cac3d89f3082bfaa03d0644
Simendi Peter <simendipeter@gmail.com>
2023-05-11 14:13:55 +0200

    refactor patient entity

--- src/main/java/zimttech/org/diabetic/screening/app/entity/Patient.java
+++ src/main/java/zimttech/org/diabetic/screening/app/entity/Patient.java
@@ -1,22 +1,50 @@
 package zimttech.org.diabetic.screening.app.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+import zimttech.org.diabetic.screening.app.entity.enums.Gender;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
+@Getter
+@Setter
+@ToString
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Builder
 public class Patient {
     @Id
     private String id;
     private String firstName;
     private String lastName;
+    @Enumerated(EnumType.STRING)
     private Gender sex;
     private LocalDate dateOfBirth;
     private String hivAidsRecordNumber;
 
+    private LocalDate dateStartedTreatment;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Patient patient = (Patient) o;
+        return getId() != null && Objects.equals(getId(), patient.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
