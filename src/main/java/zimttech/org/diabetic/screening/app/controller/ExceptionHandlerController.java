2f12c8242b4d5d8929f7068a4a4bca036ce5ccda
Simendi Peter <simendipeter@gmail.com>
2023-05-11 14:12:54 +0200

    add custom exception handling

---
+++ src/main/java/zimttech/org/diabetic/screening/app/controller/ExceptionHandlerController.java
@@ -0,0 +1,27 @@
+package zimttech.org.diabetic.screening.app.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import zimttech.org.diabetic.screening.app.exception.ValidationException;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class ExceptionHandlerController {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<String> handleValidationException(ValidationException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ src/main/java/zimttech/org/diabetic/screening/app/exception/ValidationException.java
@@ -0,0 +1,6 @@
+package zimttech.org.diabetic.screening.app.exception;
+public class ValidationException extends RuntimeException {
+    public ValidationException(String message) {
+        super(message);
+    }
+}
